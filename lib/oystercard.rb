e9e72c80382a906ec74aceb85c906106cedf3fee
India Rex <face940@gmail.com>
2020-06-15 15:50:10 +0100

    adds credit to balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,14 @@
 class Oystercard
-  
+
   DEFAULT_START_BALANCE = 0
   attr_reader :balance
-  
+
   def initialize
     @balance = DEFAULT_START_BALANCE
   end
-  
+
+  def top_up(credit)
+    @balance += credit
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,11 +1,18 @@
 require 'oystercard'
 
-describe Oystercard do 
-  
+describe Oystercard do
+
   describe '@balance' do
     it 'initializes a new card with a default balance of 0' do
       expect(subject.balance).to eq(0)
     end
   end
-  
+
+  describe '#top_up' do
+    it 'adds the specified credit to the balance' do
+      subject.top_up(9)
+      expect(subject.balance).to eq(9)
+    end
+  end
+
 end
