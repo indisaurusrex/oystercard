46dc953cff0f93c4dfed552a9a9d7212010e621e
stupot1 <scpitc@gmail.com>
2020-06-15 15:09:48 +0000

    Step 6 - Added 'over balance' error and test

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,16 @@
 class Oystercard
 
   DEFAULT_START_BALANCE = 0
+  MAXIMUM_BALANCE = 90
+  BALANCE_ERROR = "Topup will exceed maximum balance of #{MAXIMUM_BALANCE}, Topup not processed"
   attr_reader :balance
-
+  
   def initialize
     @balance = DEFAULT_START_BALANCE
   end
 
   def top_up(credit)
-    @balance += credit
+    @balance + credit > MAXIMUM_BALANCE ? raise(BALANCE_ERROR) : @balance += credit
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -13,6 +13,9 @@ describe Oystercard do
       subject.top_up(9)
       expect(subject.balance).to eq(9)
     end
+    it 'will raise an exception when a topup will exceed the maximum value of 90' do #starting with 90 but could be removed to make test better
+      expect {subject.top_up(100)}.to raise_error("Topup will exceed maximum balance of 90, Topup not processed")
+    end
   end
 
 end
