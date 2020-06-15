b05c700449c7feb18604ca722ed812bc73f11f20
stupot1 <scpitc@gmail.com>
2020-06-15 14:38:37 +0000

    Step 4 - Added balance instance variable and test to spec file

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,2 +1,10 @@
 class Oystercard
+  
+  DEFAULT_START_BALANCE = 0
+  attr_reader :balance
+  
+  def initialize
+    @balance = DEFAULT_START_BALANCE
+  end
+  
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,5 +2,10 @@ require 'oystercard'
 
 describe Oystercard do 
   
+  describe '@balance' do
+    it 'initializes a new card with a default balance of 0' do
+      expect(subject.balance).to eq(0)
+    end
+  end
   
 end
